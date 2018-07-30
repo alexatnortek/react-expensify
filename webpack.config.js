189aa4db1b697a8d6df6211597c456e4b7f99ce4
alexatnortek <alex.belyaev@nortekcontrol.com>
2018-07-30 15:44:55 -0700

    setup production build

--- webpack.config.js
+++ webpack.config.js
@@ -7,38 +7,22 @@ module.exports = (env) => {
   const CSSExtract =  new ExtractTextPlugin('styles.css');
   // console.log('env', env);
 
-  return {
-    entry: "./src/app.js",
-    output: {
-      path: path.join(__dirname, "public"),
-      filename: "bundle.js"
-    },
-    module: {
-      rules: [
-        {
-          loader: "babel-loader",
-          test: /\.js$/,
-          exclude: /node_modules/
-        },
-        {
-          test: /\.s?css$/,
-          use: CSSExtract.extract({
-            use: [
-              'css-loader',
-              'sass-loader'
-            ]
-          })
-        }
-      ]
-    },
-    plugins: [
-      CSSExtract
-    ],
-    mode: "development",
-    devtool: isProduction ? 'source-map'  : "cheap-module-eval-source-map ",
-    devServer: {
-      contentBase: path.join(__dirname, "public"),
-      historyApiFallback: true
-    }
-  }
+  return { entry: "./src/app.js", output: { path: path.join(__dirname, "public"), filename: "bundle.js" }, module: { rules: [{ loader: "babel-loader", test: /\.js$/, exclude: /node_modules/ }, { test: /\.s?css$/, use: CSSExtract.extract(
+            {
+              use: [
+                {
+                  loader: "css-loader",
+                  options: {
+                    sourceMap: true
+                  }
+                },
+                {
+                  loader: "sass-loader",
+                  options: {
+                    sourceMap: true
+                  }
+                }
+              ]
+            }
+          ) }] }, plugins: [CSSExtract], mode: "development", devtool: isProduction ? "source-map" : "inline-source-map ", devServer: { contentBase: path.join(__dirname, "public"), historyApiFallback: true } };
 }
