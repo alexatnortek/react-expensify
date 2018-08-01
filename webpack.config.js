4c16157bdef61a1705522e5e9e9d7bb318b6e999
alexatnortek <alex.belyaev@nortekcontrol.com>
2018-07-31 17:22:08 -0700

    setup dev dependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = (env) => {
   const CSSExtract =  new ExtractTextPlugin('styles.css');
   // console.log('env', env);
 
-  return { entry: "./src/app.js", output: { path: path.join(__dirname, "public"), filename: "bundle.js" }, module: { rules: [{ loader: "babel-loader", test: /\.js$/, exclude: /node_modules/ }, { test: /\.s?css$/, use: CSSExtract.extract(
+  return { entry: "./src/app.js", output: { path: path.join(__dirname, "public", "dist"), filename: "bundle.js" }, module: { rules: [{ loader: "babel-loader", test: /\.js$/, exclude: /node_modules/ }, { test: /\.s?css$/, use: CSSExtract.extract(
             {
               use: [
                 {
@@ -24,5 +24,14 @@ module.exports = (env) => {
                 }
               ]
             }
-          ) }] }, plugins: [CSSExtract], mode: "development", devtool: isProduction ? "source-map" : "inline-source-map ", devServer: { contentBase: path.join(__dirname, "public"), historyApiFallback: true } };
+          ) }] }, 
+          plugins: [CSSExtract], 
+          mode: "development", 
+          devtool: isProduction ? "source-map" : "inline-source-map ", 
+          devServer: { 
+            contentBase: path.join(__dirname, "public"), 
+            historyApiFallback: true,
+            publicPath: "/dist/" 
+          } 
+        };
 }
